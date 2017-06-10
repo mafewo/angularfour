94c455141e78c2bd8937ad4ec75b2c7007eb1c6e
mafewo <mwolters@maxisistemas.com.ar>
2017-06-10 00:45:05 -0300

    creando multiples componentes e implementando ruteador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { AlertModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './calendar/calendar.component';
+import { CoursesComponent } from './courses/courses.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'calendar', component: CalendarComponent },
+  { path: 'courses', component: CoursesComponent },
+  { path: 'home', component: HomeComponent, data: { title: 'home' }  },
+  { path: '', redirectTo: '/home', pathMatch: 'full'  },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalendarComponent
+    CalendarComponent,
+    CoursesComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     AlertModule.forRoot()
   ],
