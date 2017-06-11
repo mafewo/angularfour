6907175f438075056c6e66186ecf470b7037c5cb
mafewo <mwolters@maxisistemas.com.ar>
2017-06-11 01:35:17 -0300

    add routing file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 // firebase
 import { AngularFireAuth } from 'angularfire2/auth';
@@ -10,40 +10,29 @@ import * as firebase from 'firebase';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+@Injectable()
 export class AppComponent {
   title = 'app';
   user: Observable<firebase.User>;
   items: FirebaseListObservable<any[]>;
-  provider = new firebase.auth.FacebookAuthProvider();
+  popo;
 
   constructor(public afAuth: AngularFireAuth, db: AngularFireDatabase) {
-    this.provider.addScope('user_birthday');
-    this.user = afAuth.authState;
     this.items = db.list('items');
+    this.user = afAuth.authState;
+    this.popo = '';
   }
 
   login() {
-    firebase.auth().signInWithPopup(this.provider)
-    .then(function(result) {
-      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-      // var token = result.credential.accessToken;
-      console.log(result.credential.accessToken);
-      console.log('sisi');
-      // The signed-in user info.
-      // var user = result.user;
-      // ...
-    }).catch(function(error) {
-      console.log('errorpapa');
-      // Handle Errors here.
-      // var errorCode = error.code;
-      // var errorMessage = error.message;
-      // The email of the user's account used.
-      // var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      // var credential = error.credential;
-      // ...
+    // this.afAuth.auth.signInAnonymously();
+    this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider()).then(function(result) {
+      // console.log(result);
+      this.popo = result; // porque carajo es undefinedddD????????
+      // console.log(this.afAuth.authState);
+      // console.log(this.users);
+    }).catch(function(error){
+      console.error(error);
     });
-    // this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider());
   }
 
   logout() {
