7887b889986b79d401db1643f1837c88a4c51c9a
mafewo <mafewo@gmail.com>
2018-04-16 10:28:32 -0300

    Update app.firebaseconfig.ts

--- src/app/app.firebaseconfig.ts
+++ src/app/app.firebaseconfig.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyAHvtTzEsVrT7kPMd5GflGlLuBhngN5KiE',
+  apiKey: 'tTzEsVrT7kPMd5GflGlLuBhngN5AIzaSyAHvKiE',
   authDomain: 'hellsbells-f83fc.firebaseapp.com',
   databaseURL: 'https://hellsbells-f83fc.firebaseio.com',
   storageBucket: 'hellsbells-f83fc.appspot.com',
-  messagingSenderId: '697879315558'
+  messagingSenderId: '931555869787'
 };
 
